fea7c9d0fc0768bdf23f3bd1faa82f20cb6b4b99
Sean Costello <seancostellodp@gmail.com>
2015-02-15 16:09:40 -0500

    fixed sass task

--- gulpfile.js
+++ gulpfile.js
@@ -8,34 +8,34 @@ var concat      = require('gulp-concat');
 
 //css
 var sass        = require('gulp-ruby-sass');
-var prefixer    = require('gulp-autoprefixer');
+var prefix      = require('gulp-autoprefixer');
 
 //javascript
 var uglify      = require('gulp-uglify');
-var to5        = require('gulp-6to5');
+var to5         = require('gulp-6to5');
 var sourcemaps  = require('gulp-sourcemaps');
 
-paths = {
-  sass: 'app/**/*.sass',
-  js: 'app/index.js',
-  libs: [],
-  build: 'build'
-}
-targets = {
-  css: 'build/css',
-  js: 'build'
-}
+// paths = {
+//   sass: './app/*.sass',
+//   js: 'app/index.js',
+//   libs: [],
+//   build: 'build'
+// }
+// targets = {
+//   css: 'build/css',
+//   js: 'build'
+// }
 
 gulp.task('default', ['build', 'watch']);
 
 function generateSass () {
-  return sass(paths.sass, {quiet: true})
+  return sass('app/index.sass', {quiet: true})
     .pipe(plumber({errorHandler: utils.reportError}))
     .pipe(prefix("last 2 versions"))
-    .pipe(concat(targets.css))
+    .pipe(concat('app.css'))
     .pipe(plumber.stop())
-    .pipe(gulp.dest(paths.build))
-    .pipe(browerSync.reload({stream:true}));
+    .pipe(gulp.dest('build'))
+    .pipe(browserSync.reload({stream:true}));
 };
 
 gulp.task('sass', generateSass);
